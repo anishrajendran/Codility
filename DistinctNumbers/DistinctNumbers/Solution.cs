f0b4eedfee6975f26eb6806b3f9bdd1850caff04
Anish R <anish_rajendran@hotmail.com>
2019-08-10 22:44:49 +0530

    Initial Commit for Solution
    Distinct Numbers solution

---
+++ DistinctNumbers/DistinctNumbers/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistinctNumbers
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+        }
+    }
+}
